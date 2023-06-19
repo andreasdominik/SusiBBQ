230790c275c625ac718b0e6670e21eebb51360a4
Andreas Dominik <andreas.dominik@mni.thm.de>
2023-06-19 11:14:40 +0200

    testing

--- Skill/skill-actions.jl
+++ Skill/skill-actions.jl
@@ -25,13 +25,13 @@ function Susi_BBQtimer_action(topic, payload)
 
     print_log("action Susi_BBQtimer_action() started.")
 
-    default_time = get_config_skill(CONFIG_BBQ_TIME, default=90, cast_to=Int)
-    round_time = extract_slot_value(SLOT_BBQ_TIME, payload, 
+    @show default_time = get_config_skill(CONFIG_BBQ_TIME, default=90, cast_to=Int)
+    @show round_time = extract_slot_value(SLOT_BBQ_TIME, payload, 
                     default=default_time, as=Int)
-    wait_time = round_time / 2
+    @show wait_time = round_time / 2
 
-    sound_wav = get_config_skill(CONFIG_BBQ_SIGNAL, default="bing.wav", cast_to=String)
-    sound_wav = joinpath(get_appdir(), "assets", sound_wav)
+    @show sound_wav = get_config_skill(CONFIG_BBQ_SIGNAL, default="bing.wav", cast_to=String)
+    @show sound_wav = joinpath(get_appdir(), "assets", sound_wav)
 
     puplish_say(:bbq_info_1, round_time, :bbq_info_2, :bbq_info_3)
 
